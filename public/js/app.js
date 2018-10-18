272c4539c26a276eba601631c667438df73ecc3a
em <6em.rva@gmail.com>
2018-10-18 14:57:59 -0400

    minor adjustments

--- public/js/app.js
+++ public/js/app.js
@@ -8,41 +8,41 @@ $(document).ready(function () {
     const gn = $("#generator");
     const what = $("#what");
 
+// **************************************************************
+    
     // on click CALLBACKS button
     cb.on("click", function () {
-        
-        console.log("clicked CALLBACK");
         event.preventDefault();
         //display some text describing what's going on
-        what.text("callbacks");
-
-        // here's the callback
-        //*************************************************** 
-            //  getPhoto will find the image and then run the callback 
-            getPhoto("../")
+        what.text("callbacks"); 
     });
 
+// **************************************************************
+    
     // on click PROMISES button
     pr.on("click", function () {
-        console.log("clicked PROMISES");
         event.preventDefault();
         //display some text describing what's going on
         what.text("promises");
     });
 
+// **************************************************************
+    
     // on click ASYNC/AWAIT button
     aa.on("click", function () {
-        console.log("clicked ASYNC/AWAIT");
         event.preventDefault();
         //display some text describing what's going on
         what.text("async / await");
     });
+
+// **************************************************************
     
     // on click GENERATORS button
     gn.on("click", function () {
-        console.log("clicked GENERATOR");
         event.preventDefault();
         //display some text describing what's going on
         what.text("generator");
     });
+
+// **************************************************************
 });
