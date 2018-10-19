769b357b19e6851431a91d86d79dcc2d21c86cef
em <6em.rva@gmail.com>
2018-10-18 20:17:30 -0400

    idk

--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,58 @@
-// listening to the document 
-$(document).ready(function () {
-    console.log("app.js connected");
-    // constant variables for the different jQuery button listeners
-    const cb = $("#callback");
-    const pr = $("#promise");
-    const aa = $("#asyncAwait");
-    const gn = $("#generator");
-    const what = $("#what");
+// **************************************************************
+   
+   // listening to the document 
+        $(document).ready(function () {
+            console.log("app.js connected");
 
 // **************************************************************
     
     // on click CALLBACKS button
-    cb.on("click", function () {
+    $("#callback").on("click", function () {
         event.preventDefault();
         //display some text describing what's going on
-        what.text("callbacks"); 
+        $("#what").text("This is a callback demonstrating "); 
+    // here's the callback
+        // the callback code image is the second image in the html 
+        console.log(document.code);
+        let image = document.image[0];
+        // let downloadImage programatically create a new image
+        let downloadImage = new Image();
+        // set the handler for the onload event
+        // once the new image is loaded it will trigger a function
+        downloadImage.onload = function(){
+            // here the image source destination is named as this
+            image.src = this.src;   
+        };
+        // this is where the new image source is actually named
+        downloadImage.src = "https://media.giphy.com/media/l0HlOhvOqvyhudoWY/giphy.gif";
+
     });
 
 // **************************************************************
     
     // on click PROMISES button
-    pr.on("click", function () {
+    $("#promise").on("click", function () {
         event.preventDefault();
         //display some text describing what's going on
-        what.text("promises");
+        $("#what").text("promises");
     });
 
 // **************************************************************
     
     // on click ASYNC/AWAIT button
-    aa.on("click", function () {
+    $("#asyncAwait").on("click", function () {
         event.preventDefault();
         //display some text describing what's going on
-        what.text("async / await");
+        $("#what").text("async / await");
     });
 
 // **************************************************************
     
     // on click GENERATORS button
-    gn.on("click", function () {
+    $("#generator").on("click", function () {
         event.preventDefault();
         //display some text describing what's going on
-        what.text("generator");
+        $("#what").text("generator");
     });
 
 // **************************************************************
